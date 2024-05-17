190652e79f1e1bdfabfa8cd0b07971c919e0d4be
Dev Patel <dpatel222430@gmail.com>
2024-05-17 18:32:05 +0530

    Initial Commit

---
+++ sweater_shop/sweater_shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  sweater_shopApp.swift
+//  sweater_shop
+//
+//  Created by Dev Patel on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct sweater_shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
