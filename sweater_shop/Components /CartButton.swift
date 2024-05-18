3b9930c5d60f85e5bd046275696e26dd67e0f64c
Dev Patel <dpatel222430@gmail.com>
2024-05-18 21:42:21 +0530

    Updated UI

---
+++ sweater_shop/Components /CartButton.swift
@@ -0,0 +1,35 @@
+//
+//  CardButton.swift
+//  sweater_shop
+//
+//  Created by Dev Patel on 18/05/24.
+//
+
+import SwiftUI
+
+struct CardButton: View {
+    var numberOfProducts: Int
+    var body: some View {
+        ZStack(alignment:.topTrailing){
+            Image(systemName: "cart")
+                .font(.system(size:20))
+                .padding(.top,5)
+            
+            if numberOfProducts > 0 {
+                Text("\(numberOfProducts)")
+                    .font(.caption2)
+                    .bold()
+                    .foregroundStyle(.white)
+                    .frame(width:15,height:15)
+                    .background(.red)
+                    .cornerRadius(50)
+                
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    CardButton(numberOfProducts: 1)
+}
---
+++ sweater_shop/Views/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  sweater_shop
+//
+//  Created by Dev Patel on 18/05/24.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
